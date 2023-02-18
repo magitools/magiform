c3094e77a956766ef03767cd977a40272aab4b1b
Matteo Gassend <mgassend@gmail.com>
2023-02-18 17:00:52 +0100

    add: swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  const config = new DocumentBuilder()
+    .setTitle('MagiForm - Era')
+    .setVersion('0.0')
+    .build();
+  app.enableCors();
   const prismaService = app.get(PrismaService);
-  await prismaService.enableShutdownHooks(app)
+  await prismaService.enableShutdownHooks(app);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
