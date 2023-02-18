4dc99b8423828d3087e3356a9540b64580d7c102
matfire <mgassend@gmail.com>
2023-02-18 14:27:44 +0100

    feature: hook create/update/delete

--- src/hook/hook.service.ts
+++ src/hook/hook.service.ts
@@ -1,4 +1,5 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
@@ -9,8 +10,43 @@ export class HookService {
     private hookIntegrations,
   ) {}
 
+  private async verifyForm(userId: number, hookId: number) {
+    const form = await this.prismaService.form.findFirst({
+      where: { userId, webhooks: { some: { id: hookId } } },
+    });
+    if (!form) {
+      throw UnauthorizedException;
+    }
+  }
+
   async getTypes() {
     console.log(this.hookIntegrations);
     return this.hookIntegrations;
+  }
+
+  async create(userId: number, formId: number, data: Prisma.HookCreateInput) {
+    const form = await this.prismaService.form.findFirst({
+      where: { userId, id: formId },
+    });
+    if (!form) {
+      throw UnauthorizedException;
+    }
+    const hook = await this.prismaService.hook.create({
+      data: { forms: { connect: { id: formId } }, ...data },
+    });
+    return hook;
+  }
+
+  async delete(userId: number, hookId: number) {
+    await this.verifyForm(userId, hookId);
+    return this.prismaService.hook.delete({ where: { id: hookId } });
+  }
+
+  async update(userId: number, hookId: number, data: Prisma.HookUpdateInput) {
+    await this.verifyForm(userId, hookId);
+    return await this.prismaService.hook.update({
+      where: { id: hookId },
+      data,
+    });
   }
 }
