0ea06275f05f6dea5fb6945e6349d2799a295fb3
Matteo Gassend <mgassend@gmail.com>
2023-02-20 13:43:00 +0100

    add: form dtos

--- src/form/form.entity.ts
+++ src/form/form.entity.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Form, Hook } from '@prisma/client';
+import { Form } from '@prisma/client';
 import { HookEntity } from 'src/hook/hook.entity';
 import { StatisticsEntity } from 'src/statistics/statistic.entity';
 
