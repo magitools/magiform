13cd282814096f6c830a26d22d511550fd199e20
Matteo Gassend <mgassend@gmail.com>
2023-05-12 11:04:20 +0200

    fix: switched back to json for discord hook

--- src/integrations/Discord.hook.ts
+++ src/integrations/Discord.hook.ts
@@ -1,35 +1,43 @@
-import { Logger } from '@nestjs/common';
 import { Base } from './Base';
+import { getStoragePath } from 'src/multer/utils.multer';
 
 export default class Discord extends Base {
   constructor(url: string, data: Object, formData: string, files: Array<Express.Multer.File>) {
     super('POST', url, data, formData, files);
-    this.logger = new Logger("discord")
   }
   public async sendHook() {
-    const formData = new FormData();
     const embedFields = [];
-    const attachments = []
+    const fileFields = []
     Object.keys(this.data).forEach((e) => {
-      embedFields.push({ name: e, value: this.data[e], inline: false });
+      embedFields.push({ name: e, value: this.data[e] });
     });
     this.files.forEach((f, idx) => {
-      formData.append(`file${idx}`, new Blob([f.buffer]))
-      attachments.push({id: idx, description: f.originalname, filename: f.filename});
+      fileFields.push({name: f.originalname, value: getStoragePath(f)})
     })
-    formData.append("payload_json", JSON.stringify({embeds: embedFields, attachments, username: this.formName}));
+    const data = {
+      username: `MagiForm - ${this.formName}`,
+      embeds: [
+        {
+          title: `Fields`,
+          fields: embedFields,
+          timestamp: new Date()
+        }
+      ]
+    }
+    if (this.files.length > 0) {
+      data.embeds.push({title:`Files`, fields: fileFields, timestamp: new Date()})
+    }
     try {
       const res = await fetch(this.url, {
         method: this.method,
         headers: {
-          'Content-Type': 'multipart/form-data',
+          'Content-Type': 'application/json',
         },
-        body: formData
+        body: JSON.stringify(data)
       });
       if (!res.ok) throw(res)
     } catch (error) {
       this.logger.error(error.status)
-      console.log(formData);
       const body = await error.json()
       this.logger.error(body)
     }
